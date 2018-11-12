57ba082d685945c5c23fc091aeb1ee6c89a620fc
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-12 14:12:23 +0100

    Create grid filled with Cell objects. Controll which cell's walls should be shown.

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let cols, rows;
+let w = 60;
+let grid = [];
+
+class Cell {
+    constructor(i, j) {
+        this.i = i;
+        this.j = j;
+        this.walls = [true, true, true, true]; // top, right, bottm, left
+    }
+
+    show() {
+        let x = this.i * w;
+        let y = this.j * w;
+        stroke(255);
+        // draw walls
+        if (this.walls[0]) {
+            line(x, y, x + w, y);
+        }
+        if (this.walls[1]) {
+            line(x + w, y, x + w, y + w);
+        }
+        if (this.walls[2]) {
+            line(x + w, y + w, x, y + w);
+        }
+        if (this.walls[3]) {
+            line(x, y + w, x, y);
+        }
+    }
+
+}
+
+function setup() {
+    createCanvas(600, 600);
+    cols = Math.floor(width/w);
+    rows = Math.floor(height/w);
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            let cell = new Cell(i, j);
+            grid.push(cell);
+        }
+    }
+
+}
+
+function draw() {
+    background(0);
+
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].show();
+    }
+
+}
